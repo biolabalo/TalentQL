df2b34210ec13b2db4b5c2cca506bab75cdf8361
balogun biola <balogunbiola101@gmail.com>
2022-08-29 18:47:37 +0100

    implement functionalities

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,80 @@
+let pageNumber = 1
+interface TableRowData {
+    id: string;
+    row: number;
+    age: number;
+    gender: string;
+  }
+
+const table = document.querySelector('table')
+const tableBody = table.querySelector('tbody')
+const prevBtn: HTMLButtonElement = document.querySelector("[data-prevbtn]");
+const nextBtn: HTMLButtonElement = document.querySelector("[data-nextbtn]");
+
+const clearTableBody = () => document.querySelector('tbody').innerHTML = ''
+
+const loadIntoTable = (data: TableRowData[]) => {
+    var temp = "";
+    data.forEach((x) => {
+        temp += "<tr>";
+        temp += "<td>" + x.row + "</td>";
+        temp += "<td>" + x.gender + "</td>";
+        temp += "<td>" + x.age + "</td>";
+        temp += "</tr>"
+    });
+    clearTableBody()
+    document.querySelector('tbody').innerHTML += temp;
+}
+
+const getData = async (pageNumber: number = 1) => {
+    let url = `https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=${pageNumber}`;
+    let tableData: Array<TableRowData> = [];
+  
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      tableData = data.results[0];
+      return tableData
+
+    } catch (error) {
+        alert('Failed to fetch results')
+    }
+  };
+
+  prevBtn?.addEventListener("click", async() => {
+    if(pageNumber === 1) {
+        prevBtn.disabled = true;
+        return
+    }
+    const prevPageNumber = pageNumber
+    pageNumber  = pageNumber - 1
+    const newPageData = await getData(pageNumber)
+
+
+        //@ts-ignore
+    loadIntoTable(newPageData[pageNumber])
+  });
+
+  nextBtn?.addEventListener("click", async() => {
+    const prevPageNumber = pageNumber
+    pageNumber  = pageNumber + 1
+    const newPageData = await getData(pageNumber)
+    
+    if(prevPageNumber === 1){
+        prevBtn.disabled = false;
+    }
+
+        //@ts-ignore
+    loadIntoTable(newPageData[pageNumber])
+  });
 
 const startApp = async () => {
-    
+    prevBtn.disabled = true;
+    const pageOneandTwoData = await getData(pageNumber)
+    const pageOneData = pageOneandTwoData[1]
+    //@ts-ignore
+    loadIntoTable(pageOneData)
 };
+
 
 document.addEventListener('DOMContentLoaded', startApp);
